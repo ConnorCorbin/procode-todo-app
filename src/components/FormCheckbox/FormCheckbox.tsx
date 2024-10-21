e5c6f7dd83d847e4e25cec6187cbb3a8d06e6d2f
Connor <corbincon@outlook.com>
2024-10-21 21:01:33 +0100

    feat: form checkbox component

---
+++ src/components/FormCheckbox/FormCheckbox.tsx
@@ -0,0 +1,10 @@
+import { forwardRef } from "react";
+import { FormBaseInput, FormBaseInputProps } from "../FormBaseInput";
+
+export const FormCheckbox = forwardRef<HTMLInputElement, FormCheckboxProps>(
+  function FormCheckbox(props, ref) {
+    return <FormBaseInput ref={ref} type="checkbox" {...props} />;
+  }
+);
+
+export interface FormCheckboxProps extends Omit<FormBaseInputProps, "type"> {}
