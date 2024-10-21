7b4135980b0a7342ec7524afcdce3ff60f481340
Connor <corbincon@outlook.com>
2024-10-21 20:51:08 +0100

    refactor: paper props

--- src/components/Paper/Paper.tsx
+++ src/components/Paper/Paper.tsx
@@ -1,14 +1,13 @@
 import { forwardRef, type PropsWithChildren } from "react";
 
-export const Paper = forwardRef<HTMLDivElement, PaperProps>(function Paper(
-  { children },
-  ref
-) {
-  return (
-    <div ref={ref} className="overflow-hidden rounded-lg bg-white shadow">
-      {children}
-    </div>
-  );
-});
+export const Paper = forwardRef<HTMLDivElement, PropsWithChildren<PaperProps>>(
+  function Paper({ children }, ref) {
+    return (
+      <div ref={ref} className="overflow-hidden rounded-lg bg-white shadow">
+        {children}
+      </div>
+    );
+  }
+);
 
-export interface PaperProps extends PropsWithChildren<{}> {}
+export interface PaperProps {}
