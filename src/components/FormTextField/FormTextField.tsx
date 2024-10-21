b31c804b7d9ba6f3bda4bb1dd0b2b7a2d691e22a
Connor <corbincon@outlook.com>
2024-10-21 22:05:40 +0100

    feat: add end adornment to form text field component

--- src/components/FormTextField/FormTextField.tsx
+++ src/components/FormTextField/FormTextField.tsx
@@ -1,14 +1,25 @@
-import { forwardRef } from "react";
+import { forwardRef, type ReactNode } from "react";
 import { FormBaseInput, FormBaseInputProps } from "../FormBaseInput";
 
 export const FormTextField = forwardRef<HTMLInputElement, FormTextFieldProps>(
-  function FormTextField(props, ref) {
-    return (
+  function FormTextField({ endAdornment, ...props }, ref) {
+    const component = (
       <FormBaseInput ref={ref} className="w-full" type="text" {...props} />
+    );
+
+    if (!endAdornment) {
+      return component;
+    }
+    return (
+      <div className="flex gap-2">
+        {component}
+        {endAdornment}
+      </div>
     );
   }
 );
 
 export interface FormTextFieldProps extends Omit<FormBaseInputProps, "type"> {
   isFull?: boolean;
+  endAdornment?: ReactNode;
 }
