c6a071dfc09a82977dbf982b6ff42e82d4452800
Connor <corbincon@outlook.com>
2024-10-21 21:50:03 +0100

    feat: form field component

---
+++ src/components/FormField/FormField.tsx
@@ -0,0 +1,14 @@
+import { forwardRef, PropsWithChildren } from "react";
+
+export const FormField = forwardRef<
+  HTMLDivElement,
+  PropsWithChildren<FormFieldProps>
+>(function FormField({ children }, ref) {
+  return (
+    <div ref={ref} className="space-y-0.5">
+      {children}
+    </div>
+  );
+});
+
+export interface FormFieldProps {}
